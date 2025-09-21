f441e3751a712cd69d3fd7d4e5116c88da5bb361
rogelioperez1525401 <42542282+rogelioperez1525401@users.noreply.github.com>
2025-09-20 21:56:47 -0600

    Update PrecioGas.py

--- PrecioGas.py
+++ PrecioGas.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 
 st.write(''' # Predicción del Precio de la Gasolina con Streamlit ''')
-st.image("gasolina.jpeg", caption="Precio de gasolina regular aproximadamenteee.")
+st.image("gasolina.jpg", caption="Precio de gasolina regular aproximadamenteee.")
 
 st.header('Datos de evaluación')
 
